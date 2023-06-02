f2d83d87678ccf261c57d68934cceaea30480271
Riley <riley@dingdigital.co.uk>
2023-06-02 17:38:05 +0100

    makes text visible in inputs

--- app/helpers/url.ts
+++ app/helpers/url.ts
@@ -1,11 +1,19 @@
 import assert from "assert";
 
 export default {
+  isUrlSafe: function(name: string) {
+    if(/[^A-Za-z0-9]/.test(name)) {
+      return true;
+    }
+
+    return false;
+  },
   safeName: function(name: string) {
     assert(name, "Name is required");
     return name
       .trim()
       .toLowerCase()
       .replace(/\W+/g, '-');
+      // .replace(/[^A-Za-z0-9]/, '');
   }
 }
