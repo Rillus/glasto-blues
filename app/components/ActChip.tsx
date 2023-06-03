2b8248c926ff2612e7cac260484b87f192d49b9e
Riley <riley@dingdigital.co.uk>
2023-06-03 22:53:00 +0100

    adds loading state to stars

--- app/components/ActChip.tsx
+++ app/components/ActChip.tsx
@@ -12,6 +12,19 @@ export function ActChip(props: { name: string, id: string, isSelected: boolean }
   function addToLineup() {
     console.log('add to lineup', props.id, props.name, isSelected);
     fetcher.load(`/acts/${urlHelper.safeName(props.name)}?isSelected=${!isSelected}&actId=${props.id}`);
+    setSelectedIcon(
+      (<svg version="1.1" id="L5" x="0px" y="0px" viewBox="-20 0 100 100" style={{width: "20px", height: "20px"}} enableBackground="new 0 0 0 0">
+        <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1"></animateTransform>
+        </circle>
+        <circle fill="#fff" stroke="none" cx="30" cy="50" r="6">
+          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 10 ; 0 -10; 0 10" repeatCount="indefinite" begin="0.2"></animateTransform>
+        </circle>
+        <circle fill="#fff" stroke="none" cx="54" cy="50" r="6">
+          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5" repeatCount="indefinite" begin="0.3"></animateTransform>
+        </circle>
+      </svg>)
+    )
     return;
   }
 
