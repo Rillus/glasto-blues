3818abc5928d91d50cd8b57f0d84b65058cd3f9d
Riley <riley@dingdigital.co.uk>
2023-06-04 08:26:06 +0100

    Adds Loader component for load state Fixes acts lazy load (wasn't calculating container height) Adds orderBy name to prevent occasional duplicate entry fetching on scroll

--- app/components/ActChip.tsx
+++ app/components/ActChip.tsx
@@ -2,6 +2,7 @@ import {Link, useFetcher} from "@remix-run/react";
 import urlHelper from "~/helpers/url";
 import {useEffect, useState} from "react";
 import {useOptionalUser} from "~/utils";
+import {Loader} from "~/components/Loader";
 
 export function ActChip(props: { name: string, id: string, isSelected: boolean }) {
   const fetcher = useFetcher();
@@ -13,17 +14,7 @@ export function ActChip(props: { name: string, id: string, isSelected: boolean }
     console.log('add to lineup', props.id, props.name, isSelected);
     fetcher.load(`/acts/${urlHelper.safeName(props.name)}?isSelected=${!isSelected}&actId=${props.id}`);
     setSelectedIcon(
-      (<svg version="1.1" id="L5" x="0px" y="0px" viewBox="-20 0 100 100" style={{width: "20px", height: "20px"}} enableBackground="new 0 0 0 0">
-        <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
-          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 15 ; 0 -15; 0 15" repeatCount="indefinite" begin="0.1"></animateTransform>
-        </circle>
-        <circle fill="#fff" stroke="none" cx="30" cy="50" r="6">
-          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 10 ; 0 -10; 0 10" repeatCount="indefinite" begin="0.2"></animateTransform>
-        </circle>
-        <circle fill="#fff" stroke="none" cx="54" cy="50" r="6">
-          <animateTransform attributeName="transform" dur="1s" type="translate" values="0 5 ; 0 -5; 0 5" repeatCount="indefinite" begin="0.3"></animateTransform>
-        </circle>
-      </svg>)
+      (<Loader size={"20px"} />)
     )
     return;
   }
